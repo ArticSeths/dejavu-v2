f078e8446019bd70fcab6d9ce5d806892a60e611
Diego A <diego.alfayate55@gmail.com>
2023-06-13 19:07:26 +0200

    fix?

--- dejavu/logic/recognizer/file_recognizer.py
+++ dejavu/logic/recognizer/file_recognizer.py
@@ -2,6 +2,7 @@ from time import time
 from typing import Dict
 
 import dejavu.logic.decoder as decoder
+import numpy
 from dejavu.base_classes.base_recognizer import BaseRecognizer
 from dejavu.config.settings import (ALIGN_TIME, FINGERPRINT_TIME, QUERY_TIME,
                                     RESULTS, TOTAL_TIME)
@@ -17,6 +18,10 @@ class FileRecognizer(BaseRecognizer):
         t = time()
         matches, fingerprint_time, query_time, align_time = self._recognize(*channels)
         t = time() - t
+
+        if matches:
+            for match in matches:
+                match = {k: v.item() if isinstance(v, numpy.int64) else v for k, v in match.items()}
 
         results = {
             TOTAL_TIME: t,
